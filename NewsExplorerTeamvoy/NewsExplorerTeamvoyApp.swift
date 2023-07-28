f9216f118cca8baa3b7da4cc14f1573d43eb7b65
VanyaTobiasFate <i.kucherenko@outloud.games>
2023-07-28 15:24:17 +0300

    Initial Commit

---
+++ NewsExplorerTeamvoy/NewsExplorerTeamvoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsExplorerTeamvoyApp.swift
+//  NewsExplorerTeamvoy
+//
+//  Created by Tobias on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsExplorerTeamvoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
